be0fa55edb67887d385ea38c631caf3809375213
floriel-fedry-cko <floriel.fedry@checkout.com>
2018-05-15 17:43:21 +0100

    add test file detailsInputView

--- Tests/DetailsInputViewTests.swift
+++ Tests/DetailsInputViewTests.swift
@@ -1,5 +1,5 @@
 //
-//  StandardInputViewTests.swift
+//  DetailsInputViewTests.swift
 //  CheckoutSdkIosTests
 //
 //  Created by Floriel Fedry on 15/05/2018.
@@ -9,29 +9,29 @@
 import XCTest
 @testable import CheckoutSdkIos
 
-class StandardInputViewTests: XCTestCase {
-
+class DetailsInputViewTests: XCTestCase {
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
 
     func testEmptyInitialization() {
-        let standardInputView = StandardInputView()
+        let detailsInputView = DetailsInputView()
     }
 
     func testCoderInitialization() {
         let coder = NSKeyedUnarchiver(forReadingWith: Data())
-        let standardInputView = StandardInputView(coder: coder)
+        let detailsInputView = DetailsInputView(coder: coder)
     }
 
     func testFrameInitialization() {
-        let standardInputView = StandardInputView(frame: CGRect(x: 0, y: 0, width: 400, height: 48))
+        let detailsInputView = DetailsInputView(frame: CGRect(x: 0, y: 0, width: 400, height: 48))
     }
 
 }
